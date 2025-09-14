69fc0bea82d8b53bf18c8354d84e3093fd499b7e
Isaiah <isaiah.gamboa.383@my.csun.edu>
2025-09-13 18:42:16 -0700

    establishing file

---
+++ isaiahgamboa.c
@@ -0,0 +1,42 @@
+
+
+void Initialization(){
+
+
+}
+
+void Creation(){
+
+
+}
+
+void Destruction(){
+
+
+}
+
+void Exit(){
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+int main(){
+	
+	printf("Process creation and destruction\n");
+	printf("--------------------------------\n");
+
+
+	
+}
